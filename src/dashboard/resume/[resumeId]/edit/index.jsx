f66d361ae151a7903187c0a00867d67c5c1a8ff7
Shruti Chauhan <sc9411963207@gmail.com>
2025-02-09 11:31:33 +0530

    Manual Preview Section

--- src/dashboard/resume/[resumeId]/edit/index.jsx
+++ src/dashboard/resume/[resumeId]/edit/index.jsx
@@ -1,16 +1,28 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
+import FormSection from '../../component/FormSection';
+import ResumePreview from '../../component/ResumePreview';
+import { ResumeInfoContext } from '@/context/ResumeInfoContext';
+import dummy from '@/data/dummy';
 
 function EditResume() {
     const params=useParams();
+    const [resumeInfo,setResumeInfo]=useState();
 
     useEffect(()=>{
-        console.log(params.resumeId)
+        setResumeInfo(dummy);
+        
     },[])
   return (
-    <div>
-      EditResume
+    <ResumeInfoContext.Provider value={{resumeInfo,setResumeInfo}}>
+    <div className='grid grid-cols-1 md:grid-cols-2 p-10 gap-10'>
+      {/* Form Section */}
+      <FormSection/>
+      
+      {/* Preview Section */}
+      <ResumePreview/>
     </div>
+    </ResumeInfoContext.Provider>
   )
 }
 
---
+++ src/dashboard/resume/component/ResumePreview.jsx
@@ -0,0 +1,34 @@
+import { ResumeInfoContext } from '@/context/ResumeInfoContext'
+import React, { useContext } from 'react'
+import PersonalDetailPreview from './Preview/PersonalDetailPreview';
+import SummeryPreview from './Preview/SummeryPreview';
+import ExperiencePreview from './Preview/ExperiencePreview';
+import EducationalPreview from './Preview/EducationalPreview';
+import SkillsPreview from './Preview/SkillsPreview';
+
+function ResumePreview() {
+    const {resumeInfo,setResumeInfo}=useContext(ResumeInfoContext);
+  return (
+    <div className='shadow-lg h-full p-14 border-t-[20px]'
+    style={{
+        borderColor:resumeInfo?.themeColor
+    }}>
+      {/* Personal Detail */}
+      <PersonalDetailPreview resumeInfo={resumeInfo}/>
+
+      {/* Summary */}
+        <SummeryPreview resumeInfo={resumeInfo}/>
+
+      {/* Profrssional Experience */}
+      <ExperiencePreview resumeInfo={resumeInfo}/>
+
+      {/* Educational */}
+      <EducationalPreview resumeInfo={resumeInfo}/>
+
+      {/* Skills */}
+      <SkillsPreview resumeInfo={resumeInfo}/>
+    </div>
+  )
+}
+
+export default ResumePreview
