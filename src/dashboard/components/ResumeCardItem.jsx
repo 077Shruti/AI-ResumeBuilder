5c42c425b80f9c722ba7ba380d5bf746033a703c
Shruti Chauhan <sc9411963207@gmail.com>
2025-03-16 17:15:01 +0530

    ..

--- src/dashboard/components/ResumeCardItem.jsx
+++ src/dashboard/components/ResumeCardItem.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 
 function ResumeCardItem({resume}) {
   return (
-      <Link to={'/dashboard/resume/'+resume.resumeId+"/edit"}>
+      <Link to={'/dashboard/resume/'+resume.documentId+"/edit"}>
     
         <div className='p-14 bg-secondary flex
   items-center justify-center h-[280px] 
--- src/dashboard/resume/component/FormSection.jsx
+++ src/dashboard/resume/component/FormSection.jsx
@@ -1,10 +1,52 @@
-import React from 'react'
+import React, { useEffect, useState }  from 'react'
+import PersonalDetail from './forms/PersonalDetail'
+import { Button } from '@/components/ui/button'
+import {ArrowLeft, ArrowRight,LayoutGrid} from 'lucide-react'
+
 
 function FormSection() {
+  const [activeFormIndex,setActiveFormIndex]=useState(2);
+  const [enabledNext,setEnableNext]=useState(false)
   return (
-    <div>
-      Form Section
-    </div>
+    
+      <div>
+        <div className='flex justify-between items-center'>
+          <Button variant="outline" size="sm" className="flex-gap-2"><LayoutGrid/>Theme</Button>
+          <div className='flex gap-2'>
+            {activeFormIndex>1&&<Button size='sm'
+            onClick={()=>setActiveFormIndex(activeFormIndex-1)}  
+            ><ArrowLeft/></Button>}
+            <Button disabled={!enabledNext}
+            className="flex gap-2" size="sm"
+              onClick={()=>setActiveFormIndex(activeFormIndex+1)}
+            >Next
+              <ArrowRight/>
+            </Button>
+          </div>
+        </div>
+      
+      {/* personal Detail */}
+      {activeFormIndex==1 ?<PersonalDetail enabledNext={(v)=>setEnableNext(v)}/>
+      :null
+      }
+
+      {/* Summary */}
+
+      {/* Experience */}
+
+
+      {/* Educational */}
+
+      {/* Skills */}
+
+      
+   
+      </div>
+
+
+
+
+    
   )
 }
 
